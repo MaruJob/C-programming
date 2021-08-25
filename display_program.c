0a0a48f4ea6b674db54303100edd89e43ec0a2f0
MaruJob <ayukjob41@gmail.com>
2021-08-25 15:23:13 +0100

    Modern Approach

---
+++ display_program.c
@@ -0,0 +1,13 @@
+//Programming Project-Chapter 2-Number 1
+//A C program that uses printf to display a tick on the screen
+//Date : 25-08-2021. Time: 14:17
+#include "stdio.h"
+int main(){
+    printf("      *\n");
+    printf("     *\n");
+    printf("    *\n");
+    printf("*  *\n");
+    printf("* *\n");
+    printf(" *\n\n\n\n");
+    return 0;
+}
